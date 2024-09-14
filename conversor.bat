b1ead2d2615370c79fe0d809a1812dfb4dc4a056
Santiago Mateos <109747560+SMateosFdz@users.noreply.github.com>
2024-09-14 12:18:30 +0200

    Adding the .bat file
    This .bat file allows to execute the program from the CMD. Right now, it changes the directory of download to Videos directory

---
+++ conversor.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "C:\Users\sanma\Videos"
+@python "C:\Users\sanma\Documents\Proyectos Python\Conversor videos youtube\conversorYoutube2.py" %*
+::el %* permite coger argumentos
+@pause
+@exit
